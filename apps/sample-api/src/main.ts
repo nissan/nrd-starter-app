bc56316f238572c95d7f928275b3097ebfccce0a
Nissan Dookeran <12583+nissan@users.noreply.github.com>
2024-04-27 01:03:48 +1000

    add nestjs scaffolded app

--- apps/sample-api/src/main.ts
+++ apps/sample-api/src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // Get the port from the environment variable, defaulting to 3000 if not defined
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
