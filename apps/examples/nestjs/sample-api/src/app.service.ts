bae0fb5806c05109906887a4a74fdf45040af604
Nissan Dookeran <12583+nissan@users.noreply.github.com>
2024-05-11 09:21:56 +1000

    update package modules and default page  messages for examples

--- apps/examples/nestjs/sample-api/src/app.service.ts
+++ apps/examples/nestjs/sample-api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World from NestJS, A progressive Node.js framework for building efficient, reliable and scalable server-side applications!';
   }
 }
