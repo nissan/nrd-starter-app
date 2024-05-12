9eaee02e3955cf999f019935c3e5c8c83e76fdcd
Nissan Dookeran <12583+nissan@users.noreply.github.com>
2024-05-13 00:50:56 +1000

    add Rust Rocket framework basic app

--- apps/examples/rust/yew-app/src/main.rs
+++ apps/examples/rust/yew-app/src/main.rs
@@ -13,6 +13,7 @@ fn App() -> Html {
 
     html! {
         <div>
+            <h1>{ "Hello World from Yew, a Rust-based framework for creating reliable and efficient web applications.!" }</h1>
             <button {onclick}>{ "+1" }</button>
             <p>{ *counter }</p>
         </div>
